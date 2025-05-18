e4e85eb603e526058ec09b9f38c5400645223949
Kaedeharai <Kaedehara92693@outlook.com>
2025-05-18 22:13:30 +0800

    ModifyPreprocessingLogistic

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,8 +4,17 @@ import librosa
 
 def extract(signal, window_size=2048, hop_size=512, mel_bins=128, sample_rate=22050, fmin=0.0, fmax=None, max_frames=None, **kwargs):
     
-    stft_matrix = librosa.core.stft(y=signal, n_fft=window_size, hop_length=hop_size, window=np.hanning(window_size), center=True, dtype=np.complex64, pad_mode='reflect').T
-    melW = librosa.filters.mel(sr=sample_rate, n_fft=window_size, n_mels=mel_bins, fmin=fmin,
+    stft_matrix = librosa.core.stft(y=signal, 
+                                    n_fft=window_size, 
+                                    hop_length=hop_size, 
+                                    window=np.hanning(window_size), 
+                                    center=True, 
+                                    dtype=np.complex64, 
+                                    pad_mode='reflect').T
+    melW = librosa.filters.mel(sr=sample_rate, 
+                               n_fft=window_size, 
+                               n_mels=mel_bins, 
+                               fmin=fmin,
                                fmax=fmax).T
     
     mel_spectrogram = np.dot(np.abs(stft_matrix) ** 2, melW)
@@ -18,11 +27,9 @@ def extract(signal, window_size=2048, hop_size=512, mel_bins=128, sample_rate=22
     return logmel_spc[:-1, ]
 
 
-def mfcc(spectrum, num_filters=48, num_ceps=256):
+def mfcc(spectrum, num_ceps=256):
     
-    mfccs = np.zeros(num_ceps)
-    mel_spectrum = librosa.feature.melspectrogram(S=spectrum, n_mels=num_filters)
-    log_mel_spectrum = librosa.power_to_db(mel_spectrum)
+    log_mel_spectrum = extract(spectrum)
     mfccs = librosa.feature.mfcc(S=log_mel_spectrum, n_mfcc=num_ceps)
 
     return mfccs
@@ -39,17 +46,20 @@ def normalize_features(features):
 
 def transform_mfcc(audio):
 
-    spectrum = np.abs(librosa.stft(audio))
-    mfcc_coefficients = mfcc(spectrum)
+    mfcc_coefficients = mfcc(audio)
     normalized_mfcc_coefficients = normalize_features(mfcc_coefficients)
+    maximum_mfcc = np.max(mfcc_coefficients)
+    minimum_mfcc = np.min(mfcc_coefficients)
 
-    return normalized_mfcc_coefficients
+    return normalized_mfcc_coefficients, maximum_mfcc, minimum_mfcc
 
 
 def transform_mel(audio):
 
     mel_spectrogram = extract(audio)
     normalized_mel_spectrogram = normalize_features(mel_spectrogram)
+    maximum_mel = np.max(mel_spectrogram)
+    minimum_mel = np.min(mel_spectrogram)
 
-    return normalized_mel_spectrogram
+    return normalized_mel_spectrogram, maximum_mel, minimum_mel
 
