2cd8d29ce42f49605383596c13fa61844bb597e1
Kaedeharai <Kaedehara92693@outlook.com>
2025-05-18 22:36:47 +0800

    封装

--- train.py
+++ train.py
@@ -4,133 +4,209 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
-from logger import *
-from model import *
+from Logger import *
+from Model import *
 from DataSet import *
+from typing import Union
 from torch.utils.tensorboard import SummaryWriter
-write = SummaryWriter('runs/resnet18_binary_mfcc_sec')
 
 
-def train():
-    return
+def train_epoch(
+        epoch: int,
+        model: nn.Module, 
+        device: Union[str, torch.device],
+        loss_function: callable,
+        optimizer: optim.Optimizer,
+        training_loader: DataLoader,
+        batch_nums_per_epoch: int,
+        write: torch.utils.tensorboard.writer.SummaryWriter = None
+        )-> float: 
+    """
+    Args:
+        epoch (int): Current epoch number.
+        model (nn.Module): PyTorch model to train.
+        device (str | torch.device): Device to train on ("cuda" or "cpu").
+        loss_function (callable): Loss function to use.
+        optimizer (optim.Optimizer): Optimizer to use.
+        train_loader (DataLoader): DataLoader for training data.
+        batch_nums_per_epoch (int): Number of batches in per epoch of training set.
+        write (torch.utils.tensorboard.writer.SummaryWriter): TensorBoard writer for logging.
+    Returns:
+        loss (float): Average training loss for the epoch.
+    """
 
-def validate():
-    return
+    model.train()
+    running_loss = 0.0
+    for step, train_data in enumerate(training_loader, start=0):
+        data, labels = train_data
+        optimizer.zero_grad()
+        logits = model(data.to(device))
+
+        loss = loss_function(logits, labels.to(device))
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+        if write is not None:
+            global_update_step = epoch * batch_nums_per_epoch + step
+            write.add_scalar('per_batch_sum_train_loss', loss.item(), global_update_step)
+
+    train_loss = running_loss / batch_nums_per_epoch
+    if write is not None:
+        write.add_scalar('per_epoch_average_train_loss', train_loss, epoch)
+        write.close()
+
+    return train_loss
+
+
+def validate_epoch(
+        epoch: int,
+        model: nn.Module, 
+        device: Union[str, torch.device],
+        loss_function: callable,
+        validation_loader: DataLoader,
+        batch_nums_per_epoch: int,
+        write: torch.utils.tensorboard.writer.SummaryWriter,
+        ConfusionMatrixPath: str
+        )-> float: 
+    """
+    Args:
+        epoch (int): Current epoch number.
+        model (nn.Module): PyTorch model to validate.
+        device (str | torch.device): Device to validate on ("cuda" or "cpu").
+        loss_function (callable): Loss function to use.
+        validate_loader (DataLoader): DataLoader for validation data.
+        batch_nums_per_epoch (int): Number of batches in per epoch of validation set.
+        write (torch.utils.tensorboard.writer.SummaryWriter): TensorBoard writer for logging.
+        ConfusionMatrixPath (str): Path to save confusion matrix plots.
+    Returns:
+        validation_accuracy (float): Average validation accuracy for the epoch.
+    """
+
+    model.eval()
+    runing_accuracy = 0.0
+    runing_loss = 0.0
+    per_epoch_predict_label = []
+    per_epoch_turth_label = []
+
+    with torch.no_grad():
+        for step, val_data in enumerate(validation_loader, start=0):
+            data, ground_truth = val_data
+            logits = model(data.to(device))
+
+            predict_label = torch.argmax(logits, dim=1)
+            batch_equal_nums = torch.eq(predict_label, ground_truth.to(device)).sum().item()
+            runing_accuracy += batch_equal_nums
+
+            loss = loss_function(logits, ground_truth.to(device))
+            runing_loss += loss.item()
+
+            per_epoch_predict_label.extend(predict_label.cpu().numpy())
+            per_epoch_turth_label.extend(ground_truth.cpu().numpy())
+
+            if write is not None:
+                global_validation_step = epoch * batch_nums_per_epoch + step
+                write.add_scalar('per_batch_sum_val_accuracy', batch_equal_nums, global_validation_step)
+                write.add_scalar('per_batch_sum_val_loss', loss.item(), global_validation_step)
+
+    validation_accuracy = runing_accuracy / batch_nums_per_epoch
+    validation_loss = runing_loss / batch_nums_per_epoch
+    if write is not None:
+        write.add_scalar('per_epoch_average_val_accuracy', validation_accuracy, epoch)
+        write.add_scalar('per_epoch_average_val_loss', validation_loss, epoch)
+        write.close()
+
+    if epoch % 10 == 0 and ConfusionMatrixPath is not None:
+        cm = ConfusionMatrixCurve(per_epoch_turth_label, per_epoch_predict_label, num_classes=2)
+        cm.plot(epoch, ConfusionMatrixPath)
+
+    return validation_accuracy, validation_loss
 
 
 def main():
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    batch_size = 32
-    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 6])
-    sys.stdout = Logger("log/Train18Bin_mfcc_sec.log", sys.stdout)
 
-    method = "mel"
-    # method = "mfcc"
-
-    train_dataset = AudioDataSet(data_path=os.path.join(method, "train"),
-                                 json_path=os.path.join(method, "train.json"))
-    train_num = len(train_dataset)
-    train_loader = DataLoader(train_dataset,
-                              batch_size=batch_size,
-                              shuffle=True,
-                              num_workers=nw)
-
-    validate_dataset = AudioDataSet(data_path=os.path.join(method, "val"),
-                                    json_path=os.path.join(method, "val.json"))
-    val_num = len(validate_dataset)
-    validate_loader = DataLoader(validate_dataset,
-                                 batch_size=batch_size,
-                                 shuffle=False,
-                                 num_workers=nw)
-
-
-    net = resnet18(num_classes=2)
-    model_weight_path = "./resNet18_bin_mfcc.pth"
-
+    net, net_name = resnet50(num_classes=2)
+    model_weight_path = "pth_files/resnet50pth.pth"
     state_dict = torch.load(model_weight_path, map_location=torch.device('cpu'))
     state_dict.pop('fc.weight', None)
     state_dict.pop('fc.bias', None)
     net.load_state_dict(state_dict, strict=False)
     net.to(device)
 
+    method = "mel"
+    # method = "mfcc"
+    binary = True
+    epochs = 200
+    batch_size = 64
+    lr=0.00001
+    nw = min(os.cpu_count(), 8)
+    best_accuracy = 0.0
+
+    if binary:
+        class_num = "bin"
+    else:
+        class_num = "multi"   
+    pth_save_path = os.path.join('pth_files', f'{net_name}_{class_num}_{method}.pth')
+    sys.stdout = Logger(f"Train_{net_name}_{class_num}_{method}", sys.stdout)
+    write = SummaryWriter(os.path.join('runs', f'{net_name}_{class_num}_{method}'))
+    ConfusionMatrixPath=os.path.join('Curve', f'{net_name}_{class_num}_{method}_ConfusionMatrix')
+    os.makedirs(ConfusionMatrixPath, exist_ok=True)
 
     loss_function = nn.CrossEntropyLoss()
-    params = [p for p in net.parameters() if p.requires_grad]
-    optimizer = optim.Adam(params, lr=0.001)
+    params = net.parameters()
+    optimizer = optim.Adam(params, lr, weight_decay=1e-4)
 
-    epochs = 200
-    best_acc = 0.0
-    save_path = './resNet18_bin_mfcc_sec.pth'
+
+    train_dataset = AudioDataSet(data_path=os.path.join(method, "train"),
+                                 json_path=os.path.join(method, "train.json"),
+                                 binary=binary)
+    train_loader = DataLoader(train_dataset,
+                              batch_size=batch_size,
+                              num_workers=nw,
+                              shuffle=True,
+                              pin_memory=True)
     train_steps = len(train_loader)
-    
+
+    validate_dataset = AudioDataSet(data_path=os.path.join(method, "val"),
+                                    json_path=os.path.join(method, "val.json"),
+                                    binary=binary)
+    validate_loader = DataLoader(validate_dataset,
+                                 batch_size=batch_size,
+                                 num_workers=nw,
+                                 shuffle=False,
+                                 pin_memory=True)
+    val_steps = len(validate_loader)
+
+
     for epoch in range(epochs):
-        all_preds = []
-        all_labels = []
-        val_acc = 0.0
-        val_loss = 0.0
-        train_loss = 0.0
 
+        per_epoch_average_train_loss = train_epoch(epoch, 
+                                                   net, 
+                                                   device, 
+                                                   loss_function, 
+                                                   optimizer, 
+                                                   train_loader, 
+                                                   train_steps, 
+                                                   write)
+        print('[epoch %d] train_loss: %.3f' %(epoch + 1, per_epoch_average_train_loss))
 
-        net.train()
-        for step, data in enumerate(train_loader, start=0):
-            images, labels = data
+        validation_accuracy, validation_loss = validate_epoch(epoch, 
+                                                              net, 
+                                                              device, 
+                                                              loss_function, 
+                                                              validate_loader, 
+                                                              val_steps, 
+                                                              write,
+                                                              ConfusionMatrixPath)
+        print('[epoch %d] val_loss: %.3f val_accuracy: %.3f' %(epoch + 1, validation_loss, validation_accuracy))
 
-            optimizer.zero_grad()
-            logits = net(images.to(device))
-            loss = loss_function(logits, labels.to(device))
+        if validation_accuracy > best_accuracy:
+            best_accuracy = validation_accuracy
+            print('update best accuracy: %.3f' % best_accuracy)
+            torch.save(net.state_dict(), pth_save_path)
 
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.item()
-            write.add_scalar('perbatch_train_loss', loss.item(), epoch * train_steps + step)
-            write.add_scalar('average_train_loss_curve', train_loss / (step + 1), epoch * train_steps + step)
-            
-            print('[epoch %d, batch %d] batchloss: %.03f' %(epoch + 1, step + 1, loss.item()))
-
-
-        net.eval()
-        with torch.no_grad():
-            val_bar = tqdm(validate_loader, file=sys.stdout)
-            for val_data in val_bar:
-                val_images, val_labels = val_data
-                logits = net(val_images.to(device))
-                # probs = F.softmax(logits, dim=1)
-                _, predicted = torch.max(logits, dim=1)
-
-                perbatch_acc = torch.eq(predicted, val_labels.to(device)).sum().item()
-                write.add_scalar('perbatch_val_acc', perbatch_acc, epoch * train_steps + step)
-
-                val_acc += perbatch_acc
-                write.add_scalar('sum_acc', val_acc, epoch * train_steps + step)
-
-                loss = loss_function(logits, val_labels.to(device))
-                val_loss += loss.item()
-
-                all_preds.extend(predicted.cpu().numpy())
-                all_labels.extend(val_labels.cpu().numpy())
-                val_bar.desc = "valid epoch[{}/{}] batchloss:{:.3f}".format(epoch + 1,
-                                                                       epochs,
-                                                                       loss.item())
-
-                write.add_scalar('val_loss_curve', val_loss / len(validate_loader), epoch)
-
-        if epoch % 10 == 0:
-            cm = ConfusionMatrixCurve(all_labels, all_preds, num_classes=2)
-            cm.plot(epoch, "18_binary_mfcc_sec")
-
-
-        val_accurate = val_acc / val_num
-        write.add_scalar('val_accuracy', val_accurate, epoch)
-        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %(epoch + 1, train_loss / train_steps, val_accurate))
-
-        if val_accurate > best_acc:
-            best_acc = val_accurate
-            torch.save(net.state_dict(), save_path)
-
-    write.close()
 
 
 if __name__ == '__main__':
